3d77f5d896a9b815a554f014e4e79575849e7aba
spc009 <35326437+spc009@users.noreply.github.com>
2018-08-17 15:28:37 +0700

    edit button

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -16,5 +16,70 @@ namespace CPE200Lab1
         {
             InitializeComponent();
         }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0") lblDisplay.Text = "";
+            if(lblDisplay.Text.Length <= 8) lblDisplay.Text = lblDisplay.Text + "1";
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0") lblDisplay.Text = "";
+            if (lblDisplay.Text.Length <= 8) lblDisplay.Text = lblDisplay.Text + "2";
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0") lblDisplay.Text = "";
+            if (lblDisplay.Text.Length <= 8) lblDisplay.Text = lblDisplay.Text + "3";
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0") lblDisplay.Text = "";
+            if (lblDisplay.Text.Length <= 8) lblDisplay.Text = lblDisplay.Text + "4";
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0") lblDisplay.Text = "";
+            if (lblDisplay.Text.Length <= 8) lblDisplay.Text = lblDisplay.Text + "5";
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0") lblDisplay.Text = "";
+            if (lblDisplay.Text.Length <= 8) lblDisplay.Text = lblDisplay.Text + "6";
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0") lblDisplay.Text = "";
+            if (lblDisplay.Text.Length <= 8) lblDisplay.Text = lblDisplay.Text + "7";
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0") lblDisplay.Text = "";
+            if (lblDisplay.Text.Length <= 8) lblDisplay.Text = lblDisplay.Text + "8";
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0") lblDisplay.Text = "";
+            if (lblDisplay.Text.Length <= 8) lblDisplay.Text = lblDisplay.Text + "9";
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0") lblDisplay.Text = "";
+            if (lblDisplay.Text.Length <= 8) lblDisplay.Text = lblDisplay.Text + "0";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
