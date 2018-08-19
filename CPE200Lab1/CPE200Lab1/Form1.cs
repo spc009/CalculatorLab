b8c77a69e7c9b04fd3e65e2854ec0edf71e00dbc
spc009 <35326437+spc009@users.noreply.github.com>
2018-08-20 05:38:14 +0700

    Finish! This is nearest real calculator

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -12,74 +12,150 @@ namespace CPE200Lab1
 {
     public partial class Form1 : Form
     {
+        //initilization
+        int B = 0, stage = 1;
+        double x1, x2;
+        bool keep = false, cal = false;
+        private bool keepX1 = false, keepX2 = false;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void btn1_Click(object sender, EventArgs e)
+        //0-9 click
+        private void btnX_Click(object sender, EventArgs e)
         {
-            if (lblDisplay.Text == "0") lblDisplay.Text = "";
-            if(lblDisplay.Text.Length <= 8) lblDisplay.Text = lblDisplay.Text + "1";
+            Button btnX = (Button)sender;
+            if (stage != 1)
+            {
+                stage = 1;
+                clear();
+            }
+            if (lblDisplay.Text == "0" || keep)
+                lblDisplay.Text = "";
+            if (lblDisplay.Text.Length <= 8) lblDisplay.Text = lblDisplay.Text + btnX.Text;
+            keep = false;
+            cal = false;
+        }
+        //Calculate
+        private void Calculate(int B)
+        {
+            if (!keep)
+            {
+                if (keepX1)
+                {
+                    x2 = Convert.ToDouble(lblDisplay.Text);
+                    keepX2 = true;
+                    keep = true;
+                }
+                else
+                {
+                    x1 = Convert.ToDouble(lblDisplay.Text);
+                    keepX1 = true;
+                    keep = true;
+                }
+            }
+            if (keepX2 && keepX1)
+            {
+                switch (B)
+                {
+
+                    case 1:
+                        x1 = x1 + x2;
+                        break;
+                    case 2:
+                        x1 = x1 - x2;
+                        break;
+                    case 3:
+                        x1 = x1 * x2;
+                        break;
+                    case 4:
+                        if (x2 == 0) lblDisplay.Text = "EROR";
+                        else x1 = x1 / x2;
+                        break;
+                }
+                cal = true;
+            }
+            lblDisplay.Text = Convert.ToString(x1);
+        }
+        //Operator
+        //Equal
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            ++stage;
+            Calculate(B);
+            lblDisplay.Text = Convert.ToString(x1);
+        }
+        //Plus
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            stage = 1;
+            B = 1;
+            keepX2 = false;
+            if (!cal) Calculate(B);
+        }
+        //Sign
+        private void btnSign_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = Convert.ToString(Convert.ToDouble(lblDisplay.Text) * (-1));
+        }
+        //Minus
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            stage = 1;
+            keepX2 = false;
+            if (!cal) Calculate(B);
+            B = 2;
+        }
+        //Multiply
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            stage = 1;
+            keepX2 = false;
+            if (!cal) Calculate(B);
+            B = 3;
+        }
+        //Divide
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            stage = 1;
+            keepX2 = false;
+            if (!cal) Calculate(B);
+            B = 4;
+        }
+        //Percentage
+        private void btnPercent_Click(object sender, EventArgs e)
+        {
+            x2 = (x1 * Convert.ToDouble(lblDisplay.Text) / 100);
+            lblDisplay.Text = Convert.ToString(x2);
+        }
+        //Back
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text.Length == 1) lblDisplay.Text = "0";
+            else if (B != 0)
+            {
+                B = 0;
+                ++stage;
+            }
+            else lblDisplay.Text = lblDisplay.Text.Substring(0, lblDisplay.Text.Length - 1);
+
+        }
+        //Clear
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+        private void clear()
+        {
+            lblDisplay.Text = "0";
+            stage = 1;
+            keepX1 = false;
+            keepX2 = false;
+            keep = false;
+            B = 0;
         }
 
-        private void btn2_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0") lblDisplay.Text = "";
-            if (lblDisplay.Text.Length <= 8) lblDisplay.Text = lblDisplay.Text + "2";
-        }
-
-        private void btn3_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0") lblDisplay.Text = "";
-            if (lblDisplay.Text.Length <= 8) lblDisplay.Text = lblDisplay.Text + "3";
-        }
-
-        private void btn4_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0") lblDisplay.Text = "";
-            if (lblDisplay.Text.Length <= 8) lblDisplay.Text = lblDisplay.Text + "4";
-        }
-
-        private void btn5_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0") lblDisplay.Text = "";
-            if (lblDisplay.Text.Length <= 8) lblDisplay.Text = lblDisplay.Text + "5";
-        }
-
-        private void btn6_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0") lblDisplay.Text = "";
-            if (lblDisplay.Text.Length <= 8) lblDisplay.Text = lblDisplay.Text + "6";
-        }
-
-        private void btn7_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0") lblDisplay.Text = "";
-            if (lblDisplay.Text.Length <= 8) lblDisplay.Text = lblDisplay.Text + "7";
-        }
-
-        private void btn8_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0") lblDisplay.Text = "";
-            if (lblDisplay.Text.Length <= 8) lblDisplay.Text = lblDisplay.Text + "8";
-        }
-
-        private void btn9_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0") lblDisplay.Text = "";
-            if (lblDisplay.Text.Length <= 8) lblDisplay.Text = lblDisplay.Text + "9";
-        }
-
-        private void btn0_Click(object sender, EventArgs e)
-        {
-            if (lblDisplay.Text == "0") lblDisplay.Text = "";
-            if (lblDisplay.Text.Length <= 8) lblDisplay.Text = lblDisplay.Text + "0";
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
     }
 }
